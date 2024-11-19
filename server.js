d61a92f187a9b2b3e21d62a5727eec3e4f6a5592
Arrrbang <arem0602@gmail.com>
2024-11-19 14:22:15 +0900

    Update server.js

--- server.js
+++ server.js
@@ -5,6 +5,11 @@ app.use(express.json());
 
 const users = { user1: 'password1', admin: 'admin123' };
 
+// 루트 경로 처리
+app.get('/', (req, res) => {
+  res.send('Server is running!');  // 이 메시지가 Vercel URL의 루트 경로에서 보이게 됩니다.
+});
+
 app.post('/login', (req, res) => {
   const { username, password } = req.body;
   if (users[username] && users[username] === password) {
