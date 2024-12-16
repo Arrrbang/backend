1853ad180f16fec770a6376b976717d2174109cb
Arrrbang <arem0602@gmail.com>
2024-12-16 15:55:37 +0900

    Update index.js

--- api/index.js
+++ api/index.js
@@ -9,7 +9,7 @@ app.use(express.json());
 
 // JWT 비밀 키 (환경 변수로 설정 가능)
 const SECRET_KEY = process.env.SECRET_KEY;
-const users = JSON.parse(process.env.USERS || '{}');
+const users = { user1: 'password1', admin: 'admin123' };
 // 기본 경로
 app.get('/', (req, res) => {
   res.send('Server is running!');
